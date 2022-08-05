0f65a2a0ad6ebc3a6e3f93e846d649c5b8bcda57
leekyukin <leekujin14@gmail.com>
2022-08-06 00:45:43 +0900

    ADD :: ErrorPage

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ErrorPage() {
+  return (
+    <div className="errorContainer">
+      <div className="errorIcon">|ω・｀)ノ</div>
+      <div className="errorNum">404 error</div>
+      <div className="errorMassage">이런, 존재하지 않는 페이지입니다!</div>
+    </div>
+  );
+}
+
+export default ErrorPage;
